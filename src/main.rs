723cf7c9b27ef4f5874bba0965561cbac16da2af
mreich <reich@beontra.com>
2025-03-03 17:52:21 +0100

    finished prediction

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,87 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use perpetual::objective::Objective;
+use perpetual::{Matrix, PerpetualBooster};
+use polars::prelude::*;
+use std::sync::Arc;
+
+#[derive(Parser)]
+struct Cli {
+    budget: f32,
+}
+
+fn proba_to_indicator(prob: f64) -> u8 {
+    if prob >= 0.5 { 1 } else { 0 }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    //let budget = Cli::parse().budget;
+
+    let features_and_target = ["Survived", "Pclass", "Age", "SibSp", "Parch", "Fare"];
+
+    let features_and_target_arc: Arc<[PlSmallStr]> = features_and_target
+        .iter()
+        .map(|&s| PlSmallStr::from(s)) // Convert &str to PlSmallStr
+        .collect::<Vec<PlSmallStr>>() // Collect into Vec
+        .into_boxed_slice() // Convert Vec<PlSmallStr> to Box<[PlSmallStr]>
+        .into(); // Convert Box<[PlSmallStr]> to Arc<[PlSmallStr]>
+
+    let df = CsvReadOptions::default()
+        .with_has_header(true)
+        .with_columns(Some(features_and_target_arc))
+        .try_into_reader_with_file_path(Some("resources/titanic.csv".into()))?
+        .finish()
+        .unwrap();
+
+    println!("{}", df.head(Some(5)));
+
+    // Get data in column major format...
+    let id_vars: Vec<&str> = Vec::new();
+    let mdf = df.unpivot(["Pclass", "Age", "SibSp", "Parch", "Fare"], id_vars)?;
+
+    let data = Vec::from_iter(
+        mdf.select_at_idx(1)
+            .expect("Invalid column")
+            .f64()?
+            .into_iter()
+            .map(|v| v.unwrap_or(f64::NAN)),
+    );
+
+    let y = Vec::from_iter(
+        df.column("Survived")?
+            .cast(&DataType::Float64)?
+            .f64()?
+            .into_iter()
+            .map(|v| v.unwrap_or(f64::NAN)),
+    );
+
+    // Create Matrix from ndarray.
+    let matrix = Matrix::new(&data, y.len(), 5);
+
+    // Create booster.
+    // To provide parameters generate a default booster, and then use
+    // the relevant `set_` methods for any parameters you would like to
+    // adjust.
+    let mut model = PerpetualBooster::default()
+        .set_objective(Objective::LogLoss)
+        .set_budget(0.5);
+    model.fit(&matrix, &y, None)?;
+
+    //println!( "Model prediction(log loss): {:?} ...", &model.predict(&matrix, true)[0..10] );
+
+    let proba_results = &model.predict_proba(&matrix, true)[0..50];
+    let indicator: Vec<_> = proba_results
+        .iter()
+        .map(|&p| proba_to_indicator(p))
+        .collect();
+
+    println!("Model prediction(proba): {:?} ...", indicator);
+    println!("Reality: {:?}", &y[0..50]);
+
+    let compare = y.into_iter().zip(indicator.into_iter());
+
+    for (i, (x, y)) in compare.enumerate() {
+        println!("{}: ({}, {}, {})", i, x, y, (x - y as f64).abs());
+    }
+
+    Ok(())
 }
